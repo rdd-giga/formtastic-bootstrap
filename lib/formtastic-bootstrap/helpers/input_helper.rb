6b3852508ad7821e2f7a9086ff175cbfbd78becb
LI Daobing <lidaobing@gmail.com>
2012-09-05 15:20:53 +0800

    fix

--- lib/formtastic-bootstrap/helpers/input_helper.rb
+++ lib/formtastic-bootstrap/helpers/input_helper.rb
@@ -2,16 +2,11 @@ module FormtasticBootstrap
   module Helpers
     module InputHelper
       include Formtastic::Helpers::InputHelper
-
+      
       def standard_input_class_name(as)
-        full_name = "FormtasticBootstrap::Inputs::#{as.to_s.camelize}Input"
-        if FormtasticBootstrap::Inputs.const_defined? custom_input_class_name(as).to_sym
-          full_name
-        else
-          super as
-        end
+        "FormtasticBootstrap::Inputs::#{as.to_s.camelize}Input"
       end
-
+      
     end
   end
 end
